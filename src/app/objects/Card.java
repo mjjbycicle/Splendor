81fd74da0e9eb3f8f93ec962173235abf1ec67c1
Michael <nengzhimj@gmail.com>
2024-10-29 11:25:01 -0500

    Added Card and CardStack and ChipStack

---
+++ src/app/objects/Card.java
@@ -0,0 +1,51 @@
+package app.objects;
+
+import app.Cards;
+import app.helpers.*;
+
+public class Card {
+    private boolean faceDown;
+    private final int id;
+    private final Price price;
+    private final SingleValue value;
+    private final int points;
+
+    public Card(int id) {
+        this.id = id;
+        if (id != -1) {
+            this.faceDown = false;
+            price = Cards.cardPrices.get(id);
+            value = Cards.cardColors.get(id);
+            points = Cards.cardPoints.get(id);
+        } else {
+            this.faceDown = false;
+            price = new Price();
+            value = new SingleValue(Color.ANY, 0);
+            points = 0;
+        }
+    }
+
+    public Price getPrice() {
+        return price;
+    }
+
+    public SingleValue getValue() {
+        return value;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setFaceDown(boolean faceDown) {
+        this.faceDown = faceDown;
+    }
+
+    public boolean isFaceDown() {
+        return faceDown;
+    }
+}
